2e352519a29c0dde5b0a938ace04083d1a53720e
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2023-08-06 16:00:41 +0300

    adding new commit

--- Bash_VARs.sh
+++ Bash_VARs.sh
@@ -7,4 +7,7 @@ echo $var
 # Here It Comes to String in Bash
 STR="Welcome In Bash Mr Hammad and Try Not to Be Fast --- It always about time"
 echo $STR
+echo ${STR}
+echo ${STR:12:25}
 # End of String statement #1
+
