1e8f836a8f49092aff21cf4c97c6114ea521b47c
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2023-08-06 16:16:02 +0300

    adding new commit

--- Bash_VARs.sh
+++ Bash_VARs.sh
@@ -8,6 +8,22 @@ echo $var
 STR="Welcome In Bash Mr Hammad and Try Not to Be Fast --- It always about time"
 echo $STR
 echo ${STR}
-echo ${STR:12:25}
+echo ${STR:5:15}
 # End of String statement #1
+
+
+# Array In Bash 
+ 
+ARRAY=(one Two Three four Five)
+# Index 0  1	2	3   4
+echo ${ARRAY[@]} 
+# Gettiing all Items from the array  using @ sign.
+
+echo ${!ARRAY[@]} 
+# TO print out all Index of The Array Using ! sign.
+
+echo ${ARRAY[*]}
+# To print out all the Items of the Array Using * sign.
+
+
 
