fe6eb20b99b234e66bdab127a3a71067ea3f69c6
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2023-08-06 14:59:49 +0300

    adding new commit

---
+++ Bash_VARs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# How to insert VARS In Bash.
+var=Hello
+echo $var
+# END of VAR (1) Wonderfull.
+
+# Here It Comes to String in Bash
+STR="Welcome In Bash Mr Hammad and Try Not to Be Fast --- It always about time"
+echo $STR
+# End of String statement #1
