e266cedbb733dadb96aa7b8f463930884eca2c05
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2023-08-08 01:13:29 +0300

    My First Project Day 1

---
+++ Day1Proj.py
@@ -0,0 +1,18 @@
+# This is day 1 project
+# Band name Generator
+
+
+# Create a greeting for my program
+print("Hello This is the band name project")
+
+# Asking the user about the city he grows up.
+City_name =input("What is your city name ?")
+print(City_name)
+
+# Asking the user for his pet
+Pet_name=input("what is your pet name ?")
+print(Pet_name)
+
+# Combining the two names and printing them out.
+print("Hello Dear this is your\n " + City_name + "" + Pet_name + "\n Wow That is great ")
+
---
+++ artimatics.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Artimtics in Bash 
+CAL=$((7+10))
+echo $CAL 
+
+CAL1='expr 10+5'
+echo $CAL1
