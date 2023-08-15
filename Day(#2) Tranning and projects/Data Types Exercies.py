cf5b1ef30292cee7279330da189fff4e1d5f4edb
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2023-08-16 00:14:33 +0300

    It was a great test to have

--- Day(#2) Tranning and projects/Data Types Exercies.py
+++ Day(#2) Tranning and projects/Data Types Exercies.py
@@ -1,5 +1,9 @@
 # Day 2.1 Data types Exercise
 print("welcome to our small Program")
 New_num = input("can you Please drop your number here ")
-int(New_num)
-print(" Here is your New number \n"+ New_num )
+F_digit = New_num[0]
+S_digit = New_num[1]
+R_se=  int(F_digit) + int(S_digit)
+print("your result is the addition of the two numbers\n " + str(R_se))
+# WOW I got it.
+
