596bb675a67b7df2289ef73f423dee5e20966608
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2023-08-16 00:04:38 +0300

    adding day 2.1

---
+++ Day(#2) Tranning and projects/Data Types Exercies.py
@@ -0,0 +1,5 @@
+# Day 2.1 Data types Exercise
+print("welcome to our small Program")
+New_num = input("can you Please drop your number here ")
+int(New_num)
+print(" Here is your New number \n"+ New_num )
