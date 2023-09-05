78c2392cf32c5c77ac79e80134de4ff1a9b64529
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2023-09-05 16:03:59 +0300

    Task 4 in day 003 >> Muiltiple if statment in one

---
+++ Day(#3) COnditional Statements, Logical, code BLocks and Scopes/Multiple if statment in succession .py
@@ -0,0 +1,14 @@
+# Multiple If condition in one Tasks.
+
+Height = float(input("pls Enter your Height inn CMs :- \n "))
+if Height >= 120:
+    print("you can drive")
+    Age = int(input("Pls enter your age to determine the price\n "))
+    if Age < 12:
+        print("you're gonna cost 5$ ")
+    elif Age >= 12 & Age < 18:
+        print("you're gonna cost 7$ ")
+    elif Age >= 18:
+        print("you're gonna cost 10$  ")
+else:
+    print("you can't drive, \n we're sorry to inform you that, pls try aging. \n on the height 120 CMs")
