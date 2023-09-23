8ec7afecbb8c4fa60b7b15014a722bf78f0858fe
macrocaredc <accounts@macro.care>
2023-09-23 15:43:20 +0300

    who is going to pay :XD task 04day >> 100dayin py

---
+++ 4--Day (#4) - Beginner - Control Flow and Logical Operators/05- 005 [Interactive Coding Exercise] Banker Roulette - Who will pay the bill.py
@@ -0,0 +1,10 @@
+# Who will pay the Bill
+import random
+
+str_input = input("pass me all the names\n ")
+Ox = str_input.split(", ")
+num_list = len(Ox)
+randch = random.randint(0, num_list-1)
+personwhopays = Ox[randch]
+print(personwhopays + " he is going to pay today ")
+
