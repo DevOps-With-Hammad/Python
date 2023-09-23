beb054cac85694ddc66663e52ec88afee1155668
macrocaredc <accounts@macro.care>
2023-09-23 14:20:15 +0300

    03 Day>>004 >> 100DayinPy

---
+++ 4--Day (#4) - Beginner - Control Flow and Logical Operators/03- Heads & Tails.py
@@ -0,0 +1,8 @@
+# Heads and Tails
+import random
+
+random_size = random.randint(0, 1)
+if random_size == 1:
+    print("heads")
+else:
+    print("tails")
--- 4--Day (#4) - Beginner - Control Flow and Logical Operators/Heads and Tails .py
+++ 4--Day (#4) - Beginner - Control Flow and Logical Operators/Heads and Tails .py
@@ -2,7 +2,7 @@
 
 import random
 random_size = random.randint(0, 1)
-if random_size ==1:
+if random_size == 1:
     print(f"Here is your answer {random_size} good news you've got Head")
 else:
     print("Here is your answer you've got tail ")
