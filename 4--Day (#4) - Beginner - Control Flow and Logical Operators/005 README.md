34cbe11964effdf06d0beb779333cb7ffca92856
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2023-11-10 05:29:03 +0200

    who is gonna pay 004Day task 5

---
+++ 4--Day (#4) - Beginner - Control Flow and Logical Operators/005 README.md
@@ -0,0 +1,8 @@
+# Who Is Going To Buy The Meal Today?
+- Angela
+- Ben
+- Jeeny
+- Chole
+
+You are going to write a programme will select a random name from a list of name.
+The person selected will have to pay every body's bill of food 
---
+++ 4--Day (#4) - Beginner - Control Flow and Logical Operators/005 [Interactive Coding Exercise] Banker Roulette - Who will pay the bill.py
@@ -0,0 +1,29 @@
+# Who is Gonna Pay the full bill for our food
+# Do you know how to use the Split Function??
+
+Str_inp = "Ahmed,Ali,Mustafa,Emarra"
+op = Str_inp.split(",")
+print(op)
+# Good.
+# output should be like that
+# ['Ahmed', 'Ali', 'Mustafa', 'Emarra']
+
+# Now How about getting the names from the user using the input function.
+# >>
+name_Str = input("Pls Every Body's name Separated by comma . ")
+names = name_Str.split(",")
+print(names)
+
+# This is wonderful result to have here you really did magic today :).
+
+# Do we need the len() function ?? I don't know you tell me .
+# We also gonnaa need to Import the  RANDOM model
+import random
+Names_Str = input("Names Pls separated by comma. \n ")
+Names = Names_Str.split(",")
+print(len(Names))
+Num_of_items = len(Names)
+chois = random.randint(0, Num_of_items-1)
+print(chois)
+
+# wow You have mad it wow
