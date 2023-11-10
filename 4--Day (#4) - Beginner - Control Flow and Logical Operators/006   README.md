3e7e7c3b20b0c6900e4306396068c95ef5f12ae1
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2023-11-10 06:18:30 +0200

    task>> 006- Day>> 004- 100Day in Python *Nested list index rang error*

---
+++ 4--Day (#4) - Beginner - Control Flow and Logical Operators/006   README.md
@@ -0,0 +1,4 @@
+# 006 IndexErrors and Working with Nested Lists
+ # When you're working with nests, what is the common errors you come across?
+                                 -Index out of rang error.
+                                 -
---
+++ 4--Day (#4) - Beginner - Control Flow and Logical Operators/006 IndexErrors and Working with Nested Lists.py
@@ -0,0 +1,17 @@
+# Nested list.
+State_of_egy = ["cairo", "red sea", "port said", "Menofiya", "Sharqiya", "Daqahlyia", "Mansora",
+                "SohaJ", "Assuit", "Louxr", "Qena", "Aswan", "BanySwef", "Issmailia", "Giza", "Alx"]
+#print(State_of_egy[6])
+print(len(State_of_egy))
+# Index out of rang   error.
+#print(State_of_egy[17])
+indexx = len(State_of_egy)
+print(State_of_egy[indexx - 1])
+
+# Nested lists
+Games = ["soccer", "balls", "Basket"]
+Toys = ["chess", "snakes", "tuts"]
+full = [Games, Toys]
+print("full set is \n " + str(full))
+# WOW
+
