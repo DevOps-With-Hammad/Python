28fba0a6e2337f63e9616a3933424f1351b8b54f
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2023-11-12 00:45:49 +0200

    Task 007 >> day 004 >> 100day In Python 👽

--- 4--Day (#4) - Beginner - Control Flow and Logical Operators/007 [Interactive Coding Exercise] Treasure Map.py
+++ 4--Day (#4) - Beginner - Control Flow and Logical Operators/007 [Interactive Coding Exercise] Treasure Map.py
@@ -1,1 +1,17 @@
 #007 [Interactive Coding Exercise] Treasure Map
+row1 = ["   ", "   ", "   "]
+row2 = ["   ", "   ", "   "]
+row3 = ["   ", "   ", "   "]
+
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+
+position = input("Please Enter you position here ")
+horizontal = int(position[1])
+vertical = int(position[0])
+
+selected_row = map[vertical-1]
+selected_row[horizontal-1] = "👽"
+
+print(f"{row1}\n{row2}\n{row3}")
+
