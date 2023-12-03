49c26926ab3bc4b151f128a2076733daec8b8e2b
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2023-12-03 21:15:51 +0200

    Creating a New Branch 'Beginner Level'

--- 05 - Day 5 - Beginner - Python Loops/003 [Interactive Coding Exercise] Average Height.py
+++ 05 - Day 5 - Beginner - Python Loops/003 [Interactive Coding Exercise] Average Height.py
@@ -1,7 +1,18 @@
 # Average Height Exercise.
 # Don't change anything from the code below.
-
 Stud_Higt = input(" please Enter the Students height separated by Comma.").split()
-for n in range(2, len(Stud_Higt)):
+for n in range(0, len(Stud_Higt)):
     Stud_Higt[n] = int(Stud_Higt[n])
     print(Stud_Higt)
+# here is how to calculate the number items in here.
+print(len(Stud_Higt))
+print(sum(Stud_Higt))
+total_height = sum(Stud_Higt)
+number_of_stud = len(Stud_Higt)
+
+Total_avreage = round((total_height / number_of_stud))
+print("the AverGe of the student  = \n" + str(Total_avreage))
+
+
+
+
