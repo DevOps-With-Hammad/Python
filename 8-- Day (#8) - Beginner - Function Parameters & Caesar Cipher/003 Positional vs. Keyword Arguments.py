1ae43e3b44327a47333e6e6cd38bd79bbec1c168
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2023-12-15 06:17:18 +0200

    PYTH-13-8-D Task function with name and Depart

---
+++ 8-- Day (#8) - Beginner - Function Parameters & Caesar Cipher/003 Positional vs. Keyword Arguments.py
@@ -0,0 +1,8 @@
+# fuction with inputs
+def greeting(name, location):
+    print(f"Hello Dear \n{name}")
+    print(f"from the  {location} department")
+
+greeting("ahmed", "IT")
+greeting("Sama", "HR")
+greeting("Ayya", "Home")
