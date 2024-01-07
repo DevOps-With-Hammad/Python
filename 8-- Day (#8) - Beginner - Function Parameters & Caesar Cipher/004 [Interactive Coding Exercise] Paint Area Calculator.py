aa90d6efb4072542c1a4a31e5a8ba5d40eb19ddb
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-01-07 19:32:42 +0200

    PYTH-13-08 >> CANS for  Paint

--- 8-- Day (#8) - Beginner - Function Parameters & Caesar Cipher/004 [Interactive Coding Exercise] Paint Area Calculator.py
+++ 8-- Day (#8) - Beginner - Function Parameters & Caesar Cipher/004 [Interactive Coding Exercise] Paint Area Calculator.py
@@ -4,7 +4,7 @@ import math
 print("Welcome to, the paint wall calculator! \n Plz  fill out the requirements fields \n ")
 def paint_cal(height, width, cover):
     area = height * width
-    Num_of_can =math.ceil( area / cover)
+    Num_of_can = math.ceil(area / cover)
     print(f"You'll Need\n ({Num_of_can})Can of Paint")
 
 
@@ -12,3 +12,4 @@ test_h = int(input("Height of wall : "))
 test_w = int(input("Width of wall : "))
 covrage = 5
 paint_cal(height=test_h, width=test_w, cover=covrage)
+
