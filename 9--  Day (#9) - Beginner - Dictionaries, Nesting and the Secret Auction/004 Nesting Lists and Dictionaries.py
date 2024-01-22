a11e27a3a9a3bf5f961d61d495e039a038e394bc
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-01-22 20:48:25 +0200

    PYTH-13-08-Day>> updating day 4 with nesting dicts in lists

--- 9--  Day (#9) - Beginner - Dictionaries, Nesting and the Secret Auction/004 Nesting Lists and Dictionaries.py
+++ 9--  Day (#9) - Beginner - Dictionaries, Nesting and the Secret Auction/004 Nesting Lists and Dictionaries.py
@@ -13,15 +13,37 @@ Caps = {
 Travle_log = {
     "Egypt ": ["Cairo ", "Giza ", "ALex ", "RedSea"],
     "Plstine": ["Gaza ", "He fa", "Ya-fa ", "Rafa"],
-    "Yaman" :  ["Yaman ", "Yaman", "Yaman ", "Yaman"]
+    "Yaman": ["Taizz ", "Sana's", "Zidd ", "Aden"]
 }
- # print(Travle_log)
+# print(Travle_log)
 
 
 # Nested Dictionary in a dictionary.
 Visiting_Log = {
-    "Egypt ": {"visited_cites": ["Cairo ", "Giza ", "ALex ", "RedSea"], "Total visit":  42},
+    "Egypt ": {"visited_cites": ["Cairo ", "Giza ", "ALex ", "RedSea"], "Total visit": 42},
     "Palstine": {"Visited Cites ": ["Gaza ", "He fa", "Ya-fa ", "Rafa"], "Total Visit": 14},
-    "Yaman": {" Visited Cities ": ["Yaman ", "Yaman", "Yaman ", "Yaman"], "Total VIsit": 75}
+    "Yaman": {" Visited Cities ": ["Taizz ", "Sana's", "Zidd", "Aden"], "Total VIsit": 75}
 }
-print(Visiting_Log)
+# print(Visiting_Log)
+
+# Nesting Dict in lists
+
+travvle_trace = [
+    {
+        "Country": "Egypt",
+        "Cities ": ["Cairo", "Alex", "GIza"],
+        "Total Visit": 12
+    }
+    ,
+    {
+        "Country": "Yaman",
+        "Cities ": ["Zidd", "Sana's", "Aden"],
+        "Total Visit": 44
+    }
+    ,
+    {
+        "Country": "Palastin",
+        "Cities ": ["Gaza", "Rafa", "Hefaa"],
+        "Total Visit": 42
+    }
+]
