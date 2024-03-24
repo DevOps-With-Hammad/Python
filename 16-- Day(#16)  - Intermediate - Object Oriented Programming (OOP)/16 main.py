d23111f6aeefd26d5423feb86fc7a552711b4bbf
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-03-24 14:03:19 +0200

    PYTH-21-16 >> Creating first class using Object and class method : main and another module

---
+++ 16-- Day(#16)  - Intermediate - Object Oriented Programming (OOP)/16 main.py
@@ -0,0 +1,7 @@
+# Object and Class Creation
+import turtle
+import another_module
+
+print("Here is your answer " + str(another_module.car + another_module.merc)
+      + "\n followed by getting data from another module\n  "
+      + str(another_module.bm))
