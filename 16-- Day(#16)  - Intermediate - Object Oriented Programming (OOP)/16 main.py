f31c16226830501233ed0de4fe68a0170ed77563
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-03-24 14:23:13 +0200

    PYTH-21-16 >> Turtle method Exitonclick

--- 16-- Day(#16)  - Intermediate - Object Oriented Programming (OOP)/16 main.py
+++ 16-- Day(#16)  - Intermediate - Object Oriented Programming (OOP)/16 main.py
@@ -5,3 +5,12 @@ import another_module
 print("Here is your answer " + str(another_module.car + another_module.merc)
       + "\n followed by getting data from another module\n  "
       + str(another_module.bm))
+
+# Let's Play a little with Turtle
+from turtle import Turtle, Screen
+Hammad = Turtle()
+print("Hammad")
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
