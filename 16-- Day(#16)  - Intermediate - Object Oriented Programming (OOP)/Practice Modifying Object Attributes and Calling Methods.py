d984bdaa9402d33f0084587153572e45c080c66b
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-03-26 11:43:12 +0200

    PYTH-21-16 >> adding Align feature to the code 'L' or 'R'

--- 16-- Day(#16)  - Intermediate - Object Oriented Programming (OOP)/Practice Modifying Object Attributes and Calling Methods.py
+++ 16-- Day(#16)  - Intermediate - Object Oriented Programming (OOP)/Practice Modifying Object Attributes and Calling Methods.py
@@ -2,17 +2,18 @@
 # import prettytable
 from prettytable import PrettyTable
 
-
 table = PrettyTable()
 
-table.add_column("DATA C ",["AI ", "SE", "MP", "ARV"])
-table.add_column("Macrocare ",["Medical", "Tourism", "In", "Egypt"])
-table.add_column("DATA C ",["AI ", "SE", "MP", "ARV"])
-table.add_column("Macrocare ",["Medical", "Tourism", "In", "Egypt"])
-table.add_column("DATA C ",["AI ", "SE", "MP", "ARV"])
-table.add_column("Macrocare ",["Medical", "Tourism", "In", "Egypt"])
-table.add_column("DATA C ",["AI ", "SE", "MP", "ARV"])
-table.add_column("Macrocare ",["Medical", "Tourism", "In", "Egypt"])
+table.add_column("DATA C ", ["AI ", "SE", "MP", "ARV"])
+table.add_column("Macro care ", ["Medical", "Tourism", "In", "Egypt"])
+table.add_column("DATA C ", ["AI ", "SE", "MP", "ARV"])
+table.add_column("Macro care ", ["Medical", "Tourism", "In", "Egypt"])
+table.add_column("DATA C ", ["AI ", "SE", "MP", "ARV"])
+table.add_column("Macro care ", ["Medical", "Tourism", "In", "Egypt"])
+table.add_column("DATA C ", ["AI ", "SE", "MP", "ARV"])
+table.add_column("Macro care ", ["Medical", "Tourism", "In", "Egypt"])
 
+# TO Align Table "L" or "R", Default mode is Middle
+table.align = "l"
 
 print(table)
