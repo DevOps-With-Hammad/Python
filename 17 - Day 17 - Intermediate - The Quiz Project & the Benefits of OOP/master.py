db223bffae5ea8852ef90e15a0674b890b117323
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-04-04 22:09:53 +0200

    PYTHON-22-17 >> OOB section Day 17

--- 17 - Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/master.py
+++ 17 - Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/master.py
@@ -1,8 +1,14 @@
+
 class Phone :
     def __init__(self, app, games, other):
         self.app = app
         self.games = games
         self.other = other
+    def  catg(self, Grap, Map, Zap):
+        if Phone.company(Phone):
+            User_1.other += 0
+        else:
+            print(User_1.other, User_4.games)
 
 User_1  = Phone("Facebook", "Snake", "System")
 User_2 = Phone("WHats app", "Cat", "System")
@@ -11,7 +17,8 @@ User_4  = Phone("Twitter", "Ball", "System")
 
 print(User_1.app, User_1.games, User_1.other)
 print(User_3.games)
-print(User_2.other)
+print(User_2.other,User_2.app, User_2.other)
+print(User_4.other, User_4.app, User_4.games)
 
 # End of creation of class
 
