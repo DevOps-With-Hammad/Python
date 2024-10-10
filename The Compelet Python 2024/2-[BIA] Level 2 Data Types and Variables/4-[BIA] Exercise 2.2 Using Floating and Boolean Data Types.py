ff9d2f0dd91d38ea875ae223c0b77d6799f2eadf
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-10-10 05:00:41 +0300

    Exercise & answer  2.2 Using Floating and Boolean Data Types

---
+++ The Compelet Python 2024/2-[BIA] Level 2 Data Types and Variables/4-[BIA] Exercise 2.2 Using Floating and Boolean Data Types.py
@@ -0,0 +1,39 @@
+# 4. [BIA] Exercise 2.2 Using Floating and Boolean Data Types
+"""
+Line1 : > Define variable boolean call it as  I can't do it as False
+Line2 : > Print the Variable .
+Line3 : > Define a float variable and call  it  answer as 40.210
+Line4 : > print the variable.
+
+>> GIve it a try
+"""
+
+" define the boolean Variable  "
+I_cant_do_it = bool(False)
+print(I_cant_do_it)
+"Printing it + printing the time of this variable "
+print(type(I_cant_do_it))
+
+"""
+Output should be like >> 
+
+False
+<class 'bool'>
+
+"""
+
+"Define a float variable answer as 40.210  "
+answer = float(40.210)
+print(answer)
+"printing the variable answer value + the type ot it "
+print(type(answer))
+
+"""
+Output : >>
+
+40.21
+<class 'float'>
+
+ """
+
+"Well done , don't forget to give it a try "
