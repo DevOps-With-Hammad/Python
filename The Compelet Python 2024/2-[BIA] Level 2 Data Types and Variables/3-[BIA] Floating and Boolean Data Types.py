8533b991f5c01265dc59a3061321a33815eefad6
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-10-10 04:50:16 +0300

    Floating and Boolean Data Types

---
+++ The Compelet Python 2024/2-[BIA] Level 2 Data Types and Variables/3-[BIA] Floating and Boolean Data Types.py
@@ -0,0 +1,17 @@
+# 3. [BIA] Floating and Boolean Data Types
+Pi = 3.14
+print(Pi)
+print(type(Pi))
+Pi1 = int  (22/7)
+"Note the different here and compare it with the output "
+print(Pi1)
+print(type(Pi1))
+"Note here also the output = 7 why ?? 👆  👆  👆 "
+Pi01  = float (22/7)
+print(type(Pi01))
+" What about that? what do you think it will give the right value or not also why ? 👀👀 "
+print(Pi01)
+"3.14285"
+
+
+"Give it a try .. "
