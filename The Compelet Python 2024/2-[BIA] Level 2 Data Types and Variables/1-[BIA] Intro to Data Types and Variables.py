b1021d98418c6958a80c2109d4e5504650f4f41d
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-10-10 04:26:47 +0300

    Data types and varibles

--- The Compelet Python 2024/2-[BIA] Level 2 Data Types and Variables/1-[BIA] Intro to Data Types and Variables.py
+++ The Compelet Python 2024/2-[BIA] Level 2 Data Types and Variables/1-[BIA] Intro to Data Types and Variables.py
@@ -1,1 +1,47 @@
 # 1. [BIA] Intro to Data Types and Variables (What, Why and How of Level 2)
+"""
+ Let's declare  some Variables int str float boolean 
+"""""
+
+name = str ("name")
+print(name)
+print(type(name))
+name1 = "Python"
+print(name1)
+print(type(name1))
+"""
+print name as an output 
+ type of variable:-
+ <class 'str'>
+print as an output the word Python.
+ <class 'str'>
+ >> Give it a try .. 
+"""
+
+number1= int (12)
+number2 = 12
+print(number1)
+print(type(number1))
+print(type(number2))
+
+"""
+print name as an output 
+ type of variable:-
+ <class 'int '>
+print as an output the variable .
+ <class 'int'>
+ >> Give it a try .. 
+"""
+
+Fnum = float(20.022001)
+print(Fnum)
+print(type(Fnum))
+
+val1 = True
+val2 = False
+print(type(val1))
+print(type(val2))
+
+"""
+Practice makes perfect >> Give it a try .. 
+"""
