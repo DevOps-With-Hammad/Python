32dfc943c5bb2de909b4f858f4345392257e8cd9
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-10-10 04:37:45 +0300

    Exercise & answer 2.1 Using String and Integer Data Types

---
+++ The Compelet Python 2024/2-[BIA] Level 2 Data Types and Variables/3-[BIA] Exercise 2.1 Using String and Integer Data Types.py
@@ -0,0 +1,15 @@
+#3. [BIA] "Exercise 2.1 Using String and Integer Data Types"
+"""
+Line 1 : > Define name as string variable and call it python
+Line 2 : > Print the name
+Line 3 : > Define creation year  as an Integer variable and give it  1991 .
+Line 3 : > Print the creation Year  of this  programming Language
+.......
+Let's Code it
+
+"""
+Lang_name = "Python"
+Lang_launch_year = 1991
+print( Lang_name)
+print(Lang_launch_year)
+
