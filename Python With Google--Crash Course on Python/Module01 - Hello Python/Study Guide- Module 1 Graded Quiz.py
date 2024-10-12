a70a10cb03dd64cfa0749cbc66d6599463fc7166
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-10-13 02:10:13 +0300

    Study Guide: Module 1 Graded Quiz -- Skill 02

--- Python With Google--Crash Course on Python/Module01 - Hello Python/Study Guide- Module 1 Graded Quiz.py
+++ Python With Google--Crash Course on Python/Module01 - Hello Python/Study Guide- Module 1 Graded Quiz.py
@@ -1,7 +1,7 @@
 #Study Guide: Module 1 Graded Quiz
 # Coding Skills
-
-# Syntax for printing a string text
+#
+# Skill 01 --Syntax for printing a string text
 print("I love Python ")
 
 # Syntax for printing numeric values
@@ -12,3 +12,28 @@ print(1+1+2+7+9/2)
 # Syntax for printing the value of a variable
 value = 6*5 +2
 print(value)
+
+#Skill 02 --Use arithmetic operators, with a focus on exponents
+#
+# Multiplication, division, addition, and subtraction
+
+print(3*7/2+2+5-1) # output >>16.5
+
+# Exponents
+print(4**6) # Syntax means 4 toi the power of 6
+print(4**2) # To square a number
+print(4**3) # To cube a number
+print(4**0.5) # To find the square root of a number
+
+# to calculate how many different possible combination can be
+# formed using a set of "x" characters with each character in "x"
+# having "y" number of possible values, you will need to use an
+# exponent for the calculation
+
+x= 4
+y= 26
+print(y**6)
+
+" Give it a try , Happy coding "
+
+
