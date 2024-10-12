8efb6168a5022b6ae1f1f02e553c8c4e3c4abbbb
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-10-13 00:23:37 +0300

    M1 --  Practice Quiz: Hello World

---
+++ Python With Google--Crash Course on Python/Module01 - Hello Python/Hello World.py
@@ -0,0 +1,13 @@
+# Hell World
+print("Hello World form Python ")
+
+# A Problem form the Practice Quiz: Hello World
+
+ratio = ((1+(5**(1/2)))/2)
+print(ratio)
+
+#  calculate the square root of 9.
+#
+# Tip: to calculate the square root of a number x, you can use x**(1/2).
+x = (9**(1/2))
+print(x)
