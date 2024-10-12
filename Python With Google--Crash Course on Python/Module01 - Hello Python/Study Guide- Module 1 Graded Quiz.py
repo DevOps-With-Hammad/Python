9377cf207e930c04d2311dfef880c2eb5f56aa90
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-10-13 01:56:34 +0300

    Study Guide: Module 1 Graded Quiz >> Skill 01

---
+++ Python With Google--Crash Course on Python/Module01 - Hello Python/Study Guide- Module 1 Graded Quiz.py
@@ -0,0 +1,14 @@
+#Study Guide: Module 1 Graded Quiz
+# Coding Skills
+
+# Syntax for printing a string text
+print("I love Python ")
+
+# Syntax for printing numeric values
+print(360)
+print(3*12)
+print(1+1+2+7+9/2)
+
+# Syntax for printing the value of a variable
+value = 6*5 +2
+print(value)
