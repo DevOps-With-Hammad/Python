7439e7fb2b0ab948852bf43b0e21f29fa5bbae93
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-10-20 06:12:11 +0300

    Convert Fahrenheit to Celsius

---
+++ Python With Google--Crash Course on Python/Module01 - Basic Python Sytanx/01 - Explore Python syntax.py
@@ -0,0 +1,14 @@
+# Explore Python syntax
+# Functions: A group of related statements to perform a task and return a value
+from fileinput import close
+
+
+def to_celsius(x):
+    """ Convert Fahrenheit to Celsius"""
+    return (x-32) * 5/9
+to_celsius(75)
+to_celsius(105)
+to_celsius(35)
+print(to_celsius(15))
+print(to_celsius(95))
+print(to_celsius(125))
