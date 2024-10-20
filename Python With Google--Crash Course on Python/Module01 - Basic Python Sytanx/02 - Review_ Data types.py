0317bd072530d2d0a0ce545793b1590079453fc9
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-10-20 06:18:39 +0300

    Review : DATA Types in Python

---
+++ Python With Google--Crash Course on Python/Module01 - Basic Python Sytanx/02 - Review_ Data types.py
@@ -0,0 +1,18 @@
+# Review : Data Type
+print(7+8)
+print(type(7+8))
+###########
+print("hello "+ "world")
+print(type("hello "+ "world"))
+
+##############
+# print(7 + "8")
+#this will throw an error
+
+print(type("a"))
+print(type(2))
+print(type(2.5))
+
+print(type("a"))
+print(type(2))
+print(type(2.5))
