676bac8d62f855afd2858a457075ba7d7cc599e3
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-11-24 23:16:31 +0200

    boolean and fixing some projects files naming

--- The Compelet Python 2024/2- [BIA] Level 2 Data Types and Variables/3. [BIA] Exercise 2.1 Using String and Integer Data Types.py
+++
@@ -1,9 +0,0 @@
-# 3. [BIA] Exercise 2.1 Using String and Integer Data Types
-# Define name as a string variable & call it Python24
-# print the name string Python24
-# Define Version Integer variable and make it version 12
-# Print Version
-lang = "Python"
-print(lang)
-Lversion = 13
-print(Lversion)
