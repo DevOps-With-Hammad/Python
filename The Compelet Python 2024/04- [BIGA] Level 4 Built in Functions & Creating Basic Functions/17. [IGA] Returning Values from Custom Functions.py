2f0608380b46c1e08bdd46e617e935d0f125126e
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-12-12 13:14:45 +0200

    17. [IGA] Returning Values from Custom Functions

---
+++ The Compelet Python 2024/04- [BIGA] Level 4 Built in Functions & Creating Basic Functions/17. [IGA] Returning Values from Custom Functions.py
@@ -0,0 +1,16 @@
+# 17. [IGA] Returning Values from Custom Functions
+def convert_ksa_to_egyp(a):
+    a= a*13.25
+    print(a)
+convert_ksa_to_egyp(10)
+
+# function using return
+def convert_feht_to_c(c):
+    return (c-32)/1.8
+convert_feht_to_c(100)
+
+#
+def convert_f_to_c(c):
+    return (c-32)/1.8
+
+convert_f_to_c(100)
