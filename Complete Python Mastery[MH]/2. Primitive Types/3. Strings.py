c1b5ebf319260c78dabad42bd5b5ecf976c8b671
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-12-23 22:57:59 +0200

    # 3. Strings

--- Complete Python Mastery[MH]/2. Primitive Types/3. Strings.py
+++ Complete Python Mastery[MH]/2. Primitive Types/3. Strings.py
@@ -5,3 +5,11 @@ print(xname , yname )
 
 zname = 'Programming lang '
 print(zname)
+print(len(zname))
+# 17
+print (zname[:5])
+print (zname[3:5])
+print (zname[:11])
+print (zname[:-5])
+print (zname[:-2])
+print (zname[:-13])
