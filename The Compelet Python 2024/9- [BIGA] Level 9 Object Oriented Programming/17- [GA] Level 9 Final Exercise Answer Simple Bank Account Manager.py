ba705bfcde157d0429a910e43c5b53a8105fd93f
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-12-23 12:51:01 +0200

    # 18 [GA] Level 9 Final Exercise Simple Bank Account Manager (17- answer ) >> this code has a bug in statge 01 creating acocunt

--- The Compelet Python 2024/9- [BIGA] Level 9 Object Oriented Programming/17- [GA] Level 9 Final Exercise Answer Simple Bank Account Manager.py
+++ The Compelet Python 2024/9- [BIGA] Level 9 Object Oriented Programming/17- [GA] Level 9 Final Exercise Answer Simple Bank Account Manager.py
@@ -6,9 +6,6 @@
 - Create a dictionary called accounts to store multiple Bank account objects
 - Implement a user interface to interact with the Bank Account Objects
 """
-from math import trunc
-
-
 class BankAccount:
     def __init__(self, account_number ,balance=0):
         self.account_number = account_number
@@ -16,13 +13,35 @@ class BankAccount:
         print(f"{account_number} {balance}")
         enput = int(input("please chose form the menu: (1,2,3,4 or 5): \n "))
         self.enput = enput
+        # implementing the creation of new  account
+        # we will do it in two ways first in the __init__ main second separate method
+
+    def create_account(self,f_name, s_name, email , password ):
+        self.f_name=f_name
+        f_name= input(" your  first name: ")
+        self.s_name =s_name
+        s_name= input(" your  second name  name: ")
+        self.email =email
+        email= input(" your  email: ")
+        self.password =password
+        password = input(" your  password : ")
+        print(f"your name is >>{f_name} {s_name}\n"
+              f"With email >>{email}\n"
+              f"Ofcourse password >>{password}")
+
+
 
 
     def deposit(self, new_deposit):
 
         new_deposit = int(input("Pls, how much ? \n "))
-
-
+        print(f"Here is your deposit value {new_deposit}")
+        # check_new_deposit  = boolean(new_deposit)
+        check_new_deposit= bool(input(f"are you okay with that value{new_deposit} or you want to do somthing else( 0,1"))
+        if check_new_deposit == 0:
+            print(f"now I think your account balance has been toped up by {new_deposit}")
+        else:
+            print("Please try again later ")
 
     def withdrawal(self):
         print("Withdrawal ")
@@ -57,4 +76,4 @@ class BankAccount:
 
 account1 = BankAccount("")
 account1.ops()
-account1.deposit("")
+account1.create_account("","","","")
