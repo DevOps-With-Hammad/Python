b6312b16f179e27d68b18f42e732b56b10bce433
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-12-24 13:29:44 +0200

    # 7. Numbers

---
+++ Complete Python Mastery[MH]/2. Primitive Types/7. Numbers.py
@@ -0,0 +1,18 @@
+# 7. Numbers
+# here comes the funny part , math
+
+print(10+2)
+print(2-2)
+print(2-6)
+print(3*3)
+print(27/3)
+print(27//3, " = ")
+print(100 % 3)
+print(2**2)
+print(4**2, 4**3)
+
+x1= 10
+x =x1+3
+x1 += 3
+x1 -= 2
+print(x1 ,x ,x1 ,x1 ,x1 )
