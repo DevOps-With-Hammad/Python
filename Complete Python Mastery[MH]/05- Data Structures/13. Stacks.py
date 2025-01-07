f69eae388b6a629531f5f48e2221500636d3a4fc
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2025-01-07 13:25:35 +0200

    Stack

---
+++ Complete Python Mastery[MH]/05- Data Structures/13. Stacks.py
@@ -0,0 +1,14 @@
+# 12. Stacks >> Last in first out LIFO
+sites =[]
+sites.append(1)
+sites.append(2)
+sites.append(3)
+
+print(sites)
+
+last = sites.pop()
+print(last)
+
+print("redirect ",sites[-1])
+
+# happy coding
